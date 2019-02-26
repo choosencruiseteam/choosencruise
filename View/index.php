c9b563b5429033cffeb6e9aa9ede427cd1cb486f
Chris <ctorresj08@gmail.com>
2019-02-26 15:09:51 -0600

    Commit for fetch

--- View/index.php
+++ View/index.php
@@ -2,17 +2,18 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <script src="../Libraries/jquery-3.3.1.js"></script>
     <title>Test Title</title>
   </head>
   <body>
 
-      <!-- Test section for database functions -->
-    <script src="../View/index_ContentView"></script>
-
     <!-- This is a test, Tien -->
     <!-- Test Comment Nate -->
     <p> Hello World</p>
-    <p id="get-php"><p>
+    <div id="clicktest">Click here</div>
+
+      <!-- Test section for database functions -->
+    <script src="../View/index_ContentView_JQuery.js"></script>
 
   </body>
 </html>
