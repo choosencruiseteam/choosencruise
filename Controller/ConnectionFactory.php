7a17e272f8f42b0cb40f94c95c1aee089760f27a
Chris <ctorresj08@gmail.com>
2019-02-28 18:52:51 -0600

    Fix Include V2

--- Controller/ConnectionFactory.php
+++ Controller/ConnectionFactory.php
@@ -51,7 +51,7 @@ class ConnectionFactory
     public function getConnection()
     {
         if (is_null($this->db)) {
-            include("librarycard.php");
+            include("../Controller/librarypass.php");
             $this->db = new mysqli($host, $user, $pass, $DBName);
         }
 
