7cabeed3811575309c6f4cc91a98796982c7a5d5
Chris <ctorresj08@gmail.com>
2019-03-08 22:03:28 -0600

    Add functions to support bootstrap

--- javascript/carsearch.js
+++ javascript/carsearch.js
@@ -1,3 +1,19 @@
+//Helper function to create a new card
+function newCard(cardData) {
+
+  var template = "<div class=\"card mx-auto mb-3 \" style=\"min-width:260px;max-width:260px\">" +
+    "<img src=\"../assets/placeholder_img_v2.png\" height=\"180\" width=\"260\" class=\"card-img-top\" alt=\"...\">" +
+    "<div class=\"card-body\">" +
+    "<h5 class=\"card-title\">2019 Toyota <br>Corolla S</h5>" +
+    "<p class=\"card-title\"><u>Torres Toyota</u><br>1234 Fake St.<br>San, antonio TX 78264</p>" +
+    "<hr>" +
+    "<h5 class=\"card-text\">Posted Price:<br> $19,800</h5>" +
+    "<h5 class=\"card-text\">KBB Price:<br> $19,650</h5>" +
+    "<p class=\"card-text\"><small class=\"text-muted\">Last Update: 12/21/2049</small></p>" +
+    "</div>" +
+    "</div>";
+}
+
 $(document).ready(function() {
 
   /*******************************************
@@ -16,7 +32,7 @@ $(document).ready(function() {
     //Parse incoming data into manipulatable array
     var jsonData = JSON.parse(data);
     //Build option list for <select> element
-    var list = "<option value=\"null\">Choose...</option>";
+    var list = "<option value=\"null\" class=\"dropdown-header\">Make...</option>";
     //add returned data to option list
     for (index = 0; index < jsonData.length; ++index) {
       list += "<option value=\"" + jsonData[index] + "\">" + jsonData[index] + "</option>";
@@ -25,7 +41,7 @@ $(document).ready(function() {
     document.getElementById("make_dropdown").innerHTML = list;
 
     //Null #model_dropdown and #year_dropdown
-    var nullOption = "<option value=\"null\">-</option>";
+    var nullOption = "<option value=\"null\" class=\"dropdown-header\">-</option>";
     document.getElementById("model_dropdown").innerHTML = nullOption;
     document.getElementById("year_dropdown").innerHTML = nullOption;
 
@@ -44,7 +60,6 @@ $(document).ready(function() {
   /**************************************
             ON CHANGE LISTENERS
   ***************************************/
-
   //#make_dropdown onChange() listener
   $("#make_dropdown").change(function() {
     //Get make_dropdown value
@@ -61,7 +76,7 @@ $(document).ready(function() {
         //Parse incoming data
         var jsonData = JSON.parse(data);
         //build option list for <select> element
-        var list = "<option value=\"null\">Choose...</option>";
+        var list = "<option value=\"null\">Make...</option>";
         //add data returned from query
         for (index = 0; index < jsonData.length; ++index) {
           list += "<option value=\"" + jsonData[index] + "\">" + jsonData[index] + "</option>";
@@ -109,7 +124,7 @@ $(document).ready(function() {
         //Parse incoming data
         var jsonData = JSON.parse(data);
         //build option list for <select> element
-        var list = "<option value=\"null\">Choose...</option>";
+        var list = "<option value=\"null\">Year...</option>";
         //add data returned from query
         for (index = 0; index < jsonData.length; ++index) {
           list += "<option value=\"" + jsonData[index] + "\">" + jsonData[index] + "</option>";
@@ -131,18 +146,41 @@ $(document).ready(function() {
   /*******************************************
   ON CLICK LISTENER | search submit button
   *******************************************/
-
+  var times = 0.0;
+  var totalTime = 0.0;
   //Submit button listener
-  $("#submitCarSearch").click(function() {
-
+  $("#submitsearch").click(function() {
+    var t0 = performance.now();
     //Get search terms
     var make = $("#make_dropdown").val();
     var model = $("#model_dropdown").val();
     var year = $("#year_dropdown").val();
+    var zip = String($("#zip_textbox").val());
+    var zipRegex = /^(\d{5})$/;
 
-    var loadRow = "<tr><td colspan=11 style=\"text-align:center\" >Loading...Please wait</td></tr>";
+    /*
+      WORK IN PROGRESS -(Chris)
+      TODO:
+      -Implement regex check (done)
+      -Check input to list of Texas postal codes
+
+    console.log("\"" + zip + "\" " + zipRegex.test(zip));
+    if(zipRegex.test(zip)){
+      var dest = '78258';
+      var service = new google.maps.DistanceMatrixService();
+      service.getDistanceMatrix({
+        origins: [zip],
+        destinations: [dest],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+      }, function(data, status) {
+          alert("Status: " + status + " Data: " + JSON.stringify(data));
+      });
+    }
+
+    var loadRow = "<tr><td colspan=11 style=\"text-align:center\ >Loading...Please wait</td></tr>";
     $("#cartable").html(headerRow + loadRow);
-
+    */
     /*
       If all search terms (all dropdowns) are null, the search will default to
       getting all the cars in the database. Otherwise a search will
@@ -171,22 +209,45 @@ $(document).ready(function() {
         getString += searchTerms[i];
       }
     }
+
+
     // GET request to server for cars that will build car
     // table data.
     $.get(getString, function(data, status) {
       var jsonData = JSON.parse(data)
 
-      var body = "";
+      //US Currency Formatter
+      const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+      })
+
+      var deck = "";
       for (var i = 0; i < jsonData.length; ++i) {
-        body += "<tr>"
-        for (var j = 0; j < jsonData[i].length; ++j) {
-          body += "<td>" + jsonData[i][j] + "</td>";
-        }
-        body += "</tr>";
+        deck += "<div class=\"card mx-auto mb-3 \" style=\"min-width:260px;max-width:260px\">" +
+          "<img src=\"../assets/placeholder_img_v2.png\" height=\"180\" width=\"260\" class=\"card-img-top\" alt=\"...\">" +
+          "<div class=\"card-body\">" +
+          "<h5 class=\"card-title\">"+jsonData[i].year+" "+jsonData[i].make+"<br>"+jsonData[i].model+" "+jsonData[i].trim+"</h5>" +
+          "<p class=\"card-title\"><u>"+jsonData[i].name+"</u><br>"+jsonData[i].street+"<br>"+jsonData[i].city+" "+jsonData[i].zip+"</p>" +
+          "<hr>" +
+          "<h5 class=\"card-text\">Posted Price:<br>"+formatter.format(jsonData[i].price)+"</h5>" +
+          "<h5 class=\"card-text\">KBB Price:<br> $19,650</h5>" +
+          "<p class=\"card-text\"><small class=\"text-muted\">Last Update: "+jsonData[i].date+"</small></p>" +
+          "<input type=\"hidden\" id=\"car_id\" name=\"car_id\" value="+jsonData[i].car_id+">"+
+          "</div>" +
+          "</div>";
       }
 
-      $("#cartable").html(headerRow + body);
+      $("#card_deck").html(deck);
+      var t1 = performance.now();
+
+      totalTime += (t1 - t0);
+      times++;
+
+      console.log("Clicks: " + times + "\nAvg: " + (totalTime / times) + "ms");
     });
+
   });
 
 });
