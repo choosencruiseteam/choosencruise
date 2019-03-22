870a4b569f6d83de0d35f94ba264d9bf953423dc
Chris <ctorresj08@gmail.com>
2019-03-22 12:21:29 -0500

    Placeholder for details page

---
+++ javascript/details.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+
+  /*****************************************************************************
+                              ON PAGE LOADERS
+  *****************************************************************************/
+
+  //Load head and footer layouts
+  $('#header').load('../html/header.html');
+  $('#footer').load('../html/footer.html');
+
+
+});
