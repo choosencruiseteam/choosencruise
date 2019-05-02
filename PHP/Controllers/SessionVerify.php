7f7a6e3db0522bbe55d8caa34423ad55e586c413
Chris <ctorresj08@gmail.com>
2019-05-01 22:18:08 -0500

    Adjust functionality of session verify

--- PHP/Controllers/SessionVerify.php
+++ PHP/Controllers/SessionVerify.php
@@ -8,7 +8,7 @@ try {
     echo json_encode("Error in establishing database connection: " . $e);
 }
 
-$SESSION_TIMEOUT = 1; // 5 minute timeout
+$SESSION_TIMEOUT = 30; // 30 minute timeout
 
 if (isset($_SESSION['user']) && isset($_SESSION['auth_token']) && isset($_SESSION['last_activity'])) {
     $verifyResult = false;
@@ -30,10 +30,14 @@ if (isset($_SESSION['user']) && isset($_SESSION['auth_token']) && isset($_SESSIO
 
     if ($verifyResult != true || $timestampResult == true) {
         header("Location: http://localhost/choosencruise/html/signout.php?session_expired");
+        $_SESSION['user_auth'] = false;
         die();
     } else {
         $_SESSION['user_auth'] = true;
         $_SESSION['last_activity'] = Timestamp::now();
+        echo "<input type=\"hidden\" id=\"user_auth\" value=\"".$_SESSION['user_auth']."\">
+        <input type=\"hidden\" id=\"username_id\" value=\"".$_SESSION['id']."\">";
+
 
     }
 }
