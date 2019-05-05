06020639e35a65a4bd8fa054c1c1686d277c9122
Chris <ctorresj08@gmail.com>
2019-05-05 10:14:55 -0500

    All tabs now load data from DB

--- javascript/myaccount.js
+++ javascript/myaccount.js
@@ -20,35 +20,8 @@ $(document).ready(function() {
       $('#phone').val(user_data['phone']);
     }
   });
-  //load delivery info
-  /*
-  <tr>
-    <td>893743789</td> //order #
-    <td style="width:200px">
-      <img src="https://i.imgur.com/PUbEIhu.jpg" alt="" width="200" height="auto"> //vehicle main thumbnail
-    </td>
-    <td style="min-width:200px">
-      <div>2018 Toyota Camry R</div> // Year, Make, Model, Year, Trim
-      <hr>
-      <div><b>Final Price: $32,082</b></div> // Final price
-    </td>
-    <td style="min-width:170px">
-      <h5>Cruisin' Toyota</h5> //carlot name
-      <div>123 address Street</div> //carlot street
-      <div>San Antonio, TX 78109</div> // carlot City, state, zip
-      <div>(210)-124-9782</div> //phone number
-    </td>
-    <td style="min-width:170px">
-      <h3>Status: Pending</h3> //status
-      <hr>
-      <div>Estimated Delivery: 5 - 7 days</div> //estimated delivery
-    </td>
-    <td style="min-width:170px;max-height:250px">
-      <div>This can be used for delivery or dealership notes</div>
-    </td>
-  </tr>
-  */
 
+  //Function to create delivery table items
   $.createDeliveryTable = function(data) {
     var tableData = "";
 
@@ -91,19 +64,75 @@ $(document).ready(function() {
     });
     return tableData;
   }
-  //load account info
+  //delivery info
   var reqURL = "/choosencruise/PHP/API/get-order.php?delivery=all";
   $.get(reqURL, function(data, status) {
     var response = JSON.parse(data);
     if (response.status == true) {
       var delTableData = $.createDeliveryTable(response.result);
-      console.log(delTableData);
+      //console.log(delTableData);
       $('#delivery_table').append(delTableData);
     } else if (response.status == false) {
-      console.log("Nothing came back");
+      var emptyRow = "<tr><td colspan=\'5\'><h3>No deliveries</h3><td></tr>";
+      $('#delivery_table').append(emptyRow);
+      console.log("No delivery data present");
     }
-    $.createDeliveryTable(response);
   });
 
+  //Function to create appointment table items
+  $.createApptTable = function(data) {
+    var tableData = "";
+
+    Array.prototype.forEach.call(data, item => {
+      if(item != undefined){
+        var img;
+        if(item.main_img_hash != null){
+          img = "https://i.imgur.com/"+item.main_img_hash+".png";
+        }else{
+          img = "https://i.imgur.com/l5ysJiD.png"; //no img found src
+        }
+        var deliveryItem = "<tr>" +
+          "<td>"+item.appt_id+"</td>" + //order #
+          "<td style=\"width:200px\">" +
+          "<img src=\""+img+"\" alt=\"\" width=\"200\" height=\"auto\">" + //vehicle main thumbnail
+          "</td>" +
+          "<td style=\"min-width:200px\">" +
+          "<div>" + item.year +" "+ item.make +" "+ item.model +" "+ item.trim +"</div>" + // Year, Make, Model, Year, Trim
+          "</td>" +
+          "<td style=\"min-width:170px\">" +
+          "<h5>"+item.name+"</h5>" + //carlot name
+          "<div>"+item.street+"</div>" + //carlot street
+          "<div>"+ item.city+", "+ item.state +" "+ item.zip+"</div>" + // carlot City, state, zip
+          "<div>"+item.phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3')+"</div>" + //phone number
+          "</td>" +
+          "<td style=\"min-width:170px\">" +
+          "<h4>Appt Date: "+item.appointment_date+"</h4>" + //status
+          "<hr>" +
+          "<h5>time: "+item.time+"</h5>" + //estimated delivery
+          "</td>" +
+          "<td style=\"min-width:170px;max-height:250px\">" +
+          "<div>This can be used for delivery or dealership notes</div>" +
+          "</td>" +
+          "</tr>";
+
+        tableData += deliveryItem;
+      }
+    });
+    return tableData;
+  }
+
   //load appt info
+  var reqURL = "/choosencruise/PHP/API/get-order.php?appointment=all";
+  $.get(reqURL, function(data, status) {
+    var response = JSON.parse(data);
+    if (response.status == true) {
+      var apptTableData = $.createApptTable(response.result);
+      console.log(response);
+      $('#appointment_table').append(apptTableData);
+    } else if (response.status == false) {
+      var emptyRow = "<tr><td colspan=\'5\'><h3>No appointments</h3><td></tr>";
+      $('#appointment_table').append(emptyRow);
+      console.log("No appointment data present");
+    }
+  });
 });
